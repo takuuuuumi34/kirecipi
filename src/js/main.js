8f6e88dde9ec5cf7d605af82925d4e613f84830f
numajiri <numajiri@numajirinoMacBook-Pro.local>
2017-06-23 22:06:54 +0900

    second commit

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import App from './App.vue'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
--- src/server.py
+++ src/server.py
@@ -1,7 +1,16 @@
+import os
+import pkg_resources
 from bottle import route, run, template
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 @route('/')
 def index():
     return template('index.html')
+
+
+@route('/<dir_name>/<file_name>')
+def static_file(dir_name, file_name):
+    return pkg_resources.resource_string(__name__, dir_name+'/'+file_name)
 
 run(host="192.168.33.10", port=8080)
